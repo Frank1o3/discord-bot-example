553bed1063a409b40b46afe1bf3689f43322e97f
Frank1o3 <jahdy1o3@gmail.com>
2024-02-04 21:31:31 +0000

    modified:   Run.sh

--- Run.sh
+++ Run.sh
@@ -8,7 +8,7 @@ if [[ $MODE == "0" ]]; then
     node "$FILE"
 elif [[ $MODE == "1" ]]; then
     echo "Running with nodemon:"
-    npx nodemon "$FILE"
+    npx nodemon --inspect "$FILE"
 else
     echo "Invalid mode specified. Please use 0 for node or 1 for nodemon."
 fi
