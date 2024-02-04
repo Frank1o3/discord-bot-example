2c991daebd8f6498f80bbeec7faad3aaaba54e03
Frank1o3 <jahdy1o3@gmail.com>
2024-02-04 20:33:09 +0000

    new file:   Run.sh modified:   package.json

---
+++ Run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MODE=$1
+FILE="dist/main.js"
+
+if [[ $MODE == "0" ]]; then
+    echo "Running with node:"
+    node "$FILE"
+elif [[ $MODE == "1" ]]; then
+    echo "Running with nodemon:"
+    npx nodemon "$FILE"
+else
+    echo "Invalid mode specified. Please use 0 for node or 1 for nodemon."
+fi
